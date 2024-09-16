615ce9b0b8aacdb595cdf94d6cbf0f399db3c9f8
QuyenWibu <91710203+QuyenWibu@users.noreply.github.com>
2024-09-16 23:45:57 +0700

    cc

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import { DarkThemeToggle } from "flowbite-react";
+import "./globals.css";
+import dynamic from 'next/dynamic';
 
+const DynamicSearch= dynamic(() => import('./components/SearchBar'), { ssr: false });
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center gap-2 dark:bg-gray-800">
-      
-      <DarkThemeToggle /> 
+    <main className="flex min-h-screen items-center gap-2 dark:bg-gray-800">
+      <DynamicSearch />
     </main>
   );
 }
